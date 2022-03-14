f4d1a7fa4ce53d43ba6df95736221ea3dbcacd64
Devansh Seth <darkexists0072@gmail.com>
2022-03-14 22:38:24 +0530

    changes made: added isdiagonal func, fixes bug in isidentity func(BUG: only checking diagonal elements and working for all type of matrices #identity only works for square matrix)

--- matrix.py
+++ matrix.py
@@ -20,18 +20,37 @@ class Matrix:
             self.matrix = [[new_mat[row][col] for col in range(self.col)] for row in range(self.row)]
 
     def isidentity(self):
-        value = 1
+        value = 0
         for row in range(self.row):
             for col in range(self.col):
-                value *= self.matrix[row][col] if row==col else 1
-        return True if value==1 else False
+                if row != col:
+                    if self.matrix[row][col] != 0 : return False
+                else:
+                    if self.matrix[row][col] != 1: return False
+                    else: value += 1
+        return True if value==self.row else False
+
+    def isdiagonal(self):
+        if self.issquare:
+            dig_ele = 0
+            for row in range(self.row):
+                for col in range(self.col):
+                    if row != col:
+                        if self.matrix[row][col] !=0: return False
+                    else:
+                        dig_ele += 1 if (row==col and self.matrix[row][col] != 0) else 0
+            return True if dig_ele>0  else False
+        return False
+
 
 mat = Matrix(3,3)
 print("Old Matrix")
 for arr in mat.matrix: print(arr)
 print("Square Matrix : %s\nIdentity Matrix : %s\nOrder : %s"%(mat.issquare,mat.isidentity(),mat.order))
+print("Diagonal Matrix: %s"%(mat.isdiagonal()))
 print()
 mat.update([[1 if row==col else 0 for col in range(mat.col)]for row in range(mat.row)])
 print("New Matrix")
 for arr in mat.matrix: print(arr)
 print("Square Matrix : %s\nIdentity Matrix : %s\nOrder : %s"%(mat.issquare,mat.isidentity(),mat.order))
+print("Diagonal Matrix: %s"%(mat.isdiagonal()))
