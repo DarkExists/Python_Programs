72b2235fbb5f5509e9e5c9f0059c72100660822d
Devansh Seth <darkexists0072@gmail.com>
2022-03-14 21:12:32 +0530

    Matrix Program

--- matrix.py
+++ matrix.py
@@ -1,5 +1,9 @@
 class Matrix:
-    def __init__(self,row,col):
+    def __init__(self,row=1,col=1):
         self.row = row
         self.col = col
         self.matrix = [[0 for col in range(self.col)]for row in range(self.row)]
+
+
+mat = Matrix()
+print(mat.matrix)
